ffe79fb1215933ba9a7d2afe714506405ef369f0
nitish <nitishbabu4@gmail.com>
2023-01-18 17:15:24 +0530

    this commit for git demo

---
+++ src/test/java/com/societyManagment/expensses/testscript/DemoGithub.java
@@ -0,0 +1,10 @@
+package com.societyManagment.expensses.testscript;
+
+import org.testng.annotations.Test;
+
+public class DemoGithub {
+@Test
+public void git(){
+	System.out.println("Hello GitHub");
+}
+}
