351b2a1e81afc68fe3f8887492254ed2dcd75c37
Nitish <nitishbabu4@gmail.com>
2023-01-24 10:27:00 +0530

    There is one Method is added in Webdriver Utility i.e Fluent wait

--- src/main/java/com/societymanagement/genrics/WebDriverUtility.java
+++ src/main/java/com/societymanagement/genrics/WebDriverUtility.java
@@ -1,310 +1,328 @@
 package com.societymanagement.genrics;
 
-	import java.awt.Robot;
-	import java.awt.event.KeyEvent;
-	import java.io.File;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.Iterator;
-	import java.util.Set;
-	import java.util.concurrent.TimeUnit;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
-	import org.apache.commons.io.FileUtils;
-	import org.openqa.selenium.JavascriptExecutor;
-	import org.openqa.selenium.Keys;
-	import org.openqa.selenium.OutputType;
-	import org.openqa.selenium.TakesScreenshot;
-	import org.openqa.selenium.WebDriver;
-	import org.openqa.selenium.WebElement;
-	import org.openqa.selenium.interactions.Actions;
-	import org.openqa.selenium.support.ui.ExpectedCondition;
-	import org.openqa.selenium.support.ui.ExpectedConditions;
-	import org.openqa.selenium.support.ui.Select;
-	import org.openqa.selenium.support.ui.WebDriverWait;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * This class is used to write WebDriver WebDriver specific methods
+ * @author NitishBabu
+ *
+ */
+public class WebDriverUtility {
+	/**
+	 * This method is used to maximize the window
+	 * @author NitishBabu
+	 * @param driver
+	 */
+
+	public void maximizeWindow(WebDriver driver)
+
+	{
+		driver.manage().window().maximize();
+	}
+	/**
+	 *  Waiting 30 seconds for an element to be present on the page, checking
+			 for its presence once every 5 seconds.
+	 * @param driver
+	 * @param locators
+	 */
+	public void fluentWait(WebDriver driver,By locators) {
+		FluentWait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+				.withTimeout(Duration.ofSeconds(30))
+				.pollingEvery(Duration.ofSeconds(5))
+				.ignoring(NoSuchElementException.class);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(locators));
+
+	}
 
 	/**
-	 * This class is used to write WebDriver WebDriver specific methods
-	 * @author NitishBabu
-	 *
+	 * This method is used to wait for the page load
+	 * @author Nitish kumar
+	 * @param driver
 	 */
-	public class WebDriverUtility {
-		/**
-		 * This method is used to maximize the window
-		 * @author NitishBabu
-		 * @param driver
-		 */
-		
-		public void maximizeWindow(WebDriver driver)
-		
-		{
-			driver.manage().window().maximize();
-		}
-		
-		/**
-		 * This method is used to wait for the page load
-		 * @author Nitish kumar
-		 * @param driver
-		 */
-		public void waitForPageLaod(WebDriver driver)
-		
-		{
-			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		}
-		
-		/**
-		 * This method is used to wait until element to be visible
-		 * @param driver
-		 * @param element
-		 */
-		public void elementToBeVisible(WebDriver driver, WebElement element)
-		{
-			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-			wait.until(ExpectedConditions.visibilityOf(element));
-		}
+	public void waitForPageLaod(WebDriver driver)
 
-		/**
-		 * This method will select data from dropdown using value
-		 * @param element
-		 * @param value
-		 */
-		public void select(WebElement element, String value)
-		{
-			Select sel = new Select(element);
-			sel.selectByValue(value);
-		}
-		
-		/**
-		 * This method will select the data from dropdown using index
-		 * @param element
-		 * @param index
-		 */
-		public void select(WebElement element, int index)
-		{
-			Select sel = new Select(element);
-			sel.selectByIndex(index);
-		}
-		
-		/**
-		 * This method will select the data from dropdown using visible text
-		 * @param visibleText
-		 * @param element
-		 */
-		public void select(String visibleText, WebElement element)
-		{
-			Select sel = new Select(element);
-			sel.selectByValue(visibleText);
-		}
-		
-		/**
-		 * This method will perform MouseHover action
-		 * @param driver
-		 * @param element
-		 */
-		public void mousehover(WebDriver driver, WebElement element)
-		{
-			Actions act = new Actions(driver);
-			act.moveToElement(element).perform();
-		}
-		
-		/**
-		 * This method will perform drag and drop action
-		 * @param driver
-		 * @param src
-		 * @param dst
-		 */
-		public void dragAnddrop(WebDriver driver, WebElement src, WebElement dst)
-		{
-			Actions act = new Actions(driver);
-			act.dragAndDrop(src, dst);
-		}
-		
-		/**
-		 * This method will double click on WebPage
-		 * @param driver
-		 */
-		public void doubleClickAction(WebDriver driver)
-		{
-			Actions act= new Actions(driver);
-			act.doubleClick().perform();
-		}
-		
-		/**
-		 * This method will perform right click on WebPage
-		 * @param driver
-		 */
-		public void rightClick(WebDriver driver)
-		{
-			Actions act= new Actions(driver);
-			act.contextClick().perform();
-		}
-		
-		/**
-		 * This method will perform right click on element
-		 * @param driver
-		 * @param element
-		 */
-		
-		public void rightClick(WebDriver driver, WebElement element)
-		{
-			Actions act= new Actions(driver);
-			act.contextClick(element).perform();
-		}
-		
-		
-		/**
-		 * This method will Press Enter Key
-		 * @param driver
-		 */
-		public void enterKeyPress(WebDriver driver)
-		{
-			Actions act= new Actions(driver);
-			act.sendKeys(Keys.ENTER).perform();
-		}
-		
-		/**
-		 * This method will Press Enter Key
-		 * @param driver
-		 */
-		public void enterKey(WebDriver driver) throws Throwable
-		{
-			Robot rb= new Robot();
-			rb.keyPress(KeyEvent.VK_ENTER);
-		}
-		
-		/** 
-		 * This method is used to release the key
-		 * @param driver
-		 * @throws Throwable
-		 */
-		public void enterRelease(WebDriver driver) throws Throwable
-		{
-			Robot rb= new Robot();
-			rb.keyRelease(KeyEvent.VK_ENTER);
-		}
-		
-		
-		/**
-		 * This method will switch the frame based on index
-		 * @param driver
-		 * @param index
-		 */
-		public void switchToFrame(WebDriver driver, int index)
-		{
-			driver.switchTo().frame(index);
-		}
-		
-		
-		/**
-		 * This method will switch the frame based on nameOrID
-		 * @param driver
-		 * @param nameOrID
-		 */
-		public void switchToFrame(WebDriver driver, String nameOrID)
-		{
-			driver.switchTo().frame(nameOrID);
-		}
-		
-		/**
-		 * This method will switch the frame based on address
-		 * @param driver
-		 * @param address
-		 */
-		public void switchToFrame(WebDriver driver, WebElement address)
-		{
-			driver.switchTo().frame(address);
-		}
-		
-		/**
-		 * This method is used to accept alert popup
-		 * @param driver
-		 */
-		public void acceptAlert(WebDriver driver)
-		{
-			driver.switchTo().alert().accept();
-		}
-		
-		
-		/**
-		 * This method is used to cancel alert popup
-		 * @param driver
-		 */
-		public void cancelAlert(WebDriver driver)
-		{
-			driver.switchTo().alert().dismiss();
-		}
+	{
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
 
-		
-		/**
-		 * This method will switch between windows
-		 * @param driver
-		 * @param partialTitle
-		 */
-		public void switchToWindow(WebDriver driver, String partialTitle)
+	/**
+	 * This method is used to wait until element to be visible
+	 * @param driver
+	 * @param element
+	 */
+	public void elementToBeVisible(WebDriver driver, WebElement element)
+	{
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.visibilityOf(element));
+	}
+
+	/**
+	 * This method will select data from dropdown using value
+	 * @param element
+	 * @param value
+	 */
+	public void select(WebElement element, String value)
+	{
+		Select sel = new Select(element);
+		sel.selectByValue(value);
+	}
+
+	/**
+	 * This method will select the data from dropdown using index
+	 * @param element
+	 * @param index
+	 */
+	public void select(WebElement element, int index)
+	{
+		Select sel = new Select(element);
+		sel.selectByIndex(index);
+	}
+
+	/**
+	 * This method will select the data from dropdown using visible text
+	 * @param visibleText
+	 * @param element
+	 */
+	public void select(String visibleText, WebElement element)
+	{
+		Select sel = new Select(element);
+		sel.selectByValue(visibleText);
+	}
+
+	/**
+	 * This method will perform MouseHover action
+	 * @param driver
+	 * @param element
+	 */
+	public void mousehover(WebDriver driver, WebElement element)
+	{
+		Actions act = new Actions(driver);
+		act.moveToElement(element).perform();
+	}
+
+	/**
+	 * This method will perform drag and drop action
+	 * @param driver
+	 * @param src
+	 * @param dst
+	 */
+	public void dragAnddrop(WebDriver driver, WebElement src, WebElement dst)
+	{
+		Actions act = new Actions(driver);
+		act.dragAndDrop(src, dst);
+	}
+
+	/**
+	 * This method will double click on WebPage
+	 * @param driver
+	 */
+	public void doubleClickAction(WebDriver driver)
+	{
+		Actions act= new Actions(driver);
+		act.doubleClick().perform();
+	}
+
+	/**
+	 * This method will perform right click on WebPage
+	 * @param driver
+	 */
+	public void rightClick(WebDriver driver)
+	{
+		Actions act= new Actions(driver);
+		act.contextClick().perform();
+	}
+
+	/**
+	 * This method will perform right click on element
+	 * @param driver
+	 * @param element
+	 */
+
+	public void rightClick(WebDriver driver, WebElement element)
+	{
+		Actions act= new Actions(driver);
+		act.contextClick(element).perform();
+	}
+
+
+	/**
+	 * This method will Press Enter Key
+	 * @param driver
+	 */
+	public void enterKeyPress(WebDriver driver)
+	{
+		Actions act= new Actions(driver);
+		act.sendKeys(Keys.ENTER).perform();
+	}
+
+	/**
+	 * This method will Press Enter Key
+	 * @param driver
+	 */
+	public void enterKey(WebDriver driver) throws Throwable
+	{
+		Robot rb= new Robot();
+		rb.keyPress(KeyEvent.VK_ENTER);
+	}
+
+	/** 
+	 * This method is used to release the key
+	 * @param driver
+	 * @throws Throwable
+	 */
+	public void enterRelease(WebDriver driver) throws Throwable
+	{
+		Robot rb= new Robot();
+		rb.keyRelease(KeyEvent.VK_ENTER);
+	}
+
+
+	/**
+	 * This method will switch the frame based on index
+	 * @param driver
+	 * @param index
+	 */
+	public void switchToFrame(WebDriver driver, int index)
+	{
+		driver.switchTo().frame(index);
+	}
+
+
+	/**
+	 * This method will switch the frame based on nameOrID
+	 * @param driver
+	 * @param nameOrID
+	 */
+	public void switchToFrame(WebDriver driver, String nameOrID)
+	{
+		driver.switchTo().frame(nameOrID);
+	}
+
+	/**
+	 * This method will switch the frame based on address
+	 * @param driver
+	 * @param address
+	 */
+	public void switchToFrame(WebDriver driver, WebElement address)
+	{
+		driver.switchTo().frame(address);
+	}
+
+	/**
+	 * This method is used to accept alert popup
+	 * @param driver
+	 */
+	public void acceptAlert(WebDriver driver)
+	{
+		driver.switchTo().alert().accept();
+	}
+
+
+	/**
+	 * This method is used to cancel alert popup
+	 * @param driver
+	 */
+	public void cancelAlert(WebDriver driver)
+	{
+		driver.switchTo().alert().dismiss();
+	}
+
+
+	/**
+	 * This method will switch between windows
+	 * @param driver
+	 * @param partialTitle
+	 */
+	public void switchToWindow(WebDriver driver, String partialTitle)
+	{
+		//step1: use getWindowHandes to capture all window id's
+		Set<String> windows = driver.getWindowHandles();
+
+		//step2: iterate through the windows
+		Iterator<String> it = windows.iterator();
+
+		//step3: check whether there is next window
+		while(it.hasNext())
 		{
-			//step1: use getWindowHandes to capture all window id's
-			Set<String> windows = driver.getWindowHandles();
-			
-			//step2: iterate through the windows
-			Iterator<String> it = windows.iterator();
-			
-			//step3: check whether there is next window
-			while(it.hasNext())
+			//step4: capture current window id
+			String winId = it.next();
+
+			//step5: switch to current window and capture title 
+			String currentWinTitle = driver.switchTo().window(winId).getTitle();
+
+			//step6: check whether current window is expected
+			if(currentWinTitle.contains(partialTitle))
 			{
-				//step4: capture current window id
-				String winId = it.next();
-				
-				//step5: switch to current window and capture title 
-				String currentWinTitle = driver.switchTo().window(winId).getTitle();
-				
-				//step6: check whether current window is expected
-				if(currentWinTitle.contains(partialTitle))
-				{
-					break;
-				}
+				break;
 			}
-		}
-		
-		
-		/**
-		 * This method will take screenshot and store it in folder called as screenshot
-		 * @param driver
-		 * @param screenShotName
-		 * @return
-		 * @throws Throwable
-		 */
-		public static String getScreenShot(WebDriver driver ,String screenShotName) throws Throwable
-			{
-				TakesScreenshot ts = (TakesScreenshot) driver;
-				File src = ts.getScreenshotAs(OutputType.FILE);
-				String path = ".\\screenshot\\"+screenShotName+".png";
-				File dst = new File(path);
-				FileUtils.copyFile(src, dst);
-				return path;
-				
-			}
-		
-		/**
-		 * This method will perform random scroll
-		 * @param driver
-		 */
-		public void scrollBarAction(WebDriver driver)
-		{
-			JavascriptExecutor jse = (JavascriptExecutor) driver;
-			jse.executeScript("window.scrollBy(0,800)","");
-		}
-		
-		
-		/**
-		 * This method will scroll until specified element is found
-		 * @param driver
-		 * @param element
-		 */
-		public void scrollAction(WebDriver driver, WebElement element)
-		{
-			JavascriptExecutor jse = (JavascriptExecutor) driver;
-			int y = element.getLocation().getY();
-			jse.executeScript("window.scrollBy(0,"+y+")", element);
-			//jse.executeScript("argument[0].scrollIntoView()", element);
 		}
 	}
+
+
+	/**
+	 * This method will take screenshot and store it in folder called as screenshot
+	 * @param driver
+	 * @param screenShotName
+	 * @return
+	 * @throws Throwable
+	 */
+	public static String getScreenShot(WebDriver driver ,String screenShotName) throws Throwable
+	{
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File src = ts.getScreenshotAs(OutputType.FILE);
+		String path = ".\\screenshot\\"+screenShotName+".png";
+		File dst = new File(path);
+		FileUtils.copyFile(src, dst);
+		return path;
+
+	}
+
+	/**
+	 * This method will perform random scroll
+	 * @param driver
+	 */
+	public void scrollBarAction(WebDriver driver)
+	{
+		JavascriptExecutor jse = (JavascriptExecutor) driver;
+		jse.executeScript("window.scrollBy(0,800)","");
+	}
+
+
+	/**
+	 * This method will scroll until specified element is found
+	 * @param driver
+	 * @param element
+	 */
+	public void scrollAction(WebDriver driver, WebElement element)
+	{
+		JavascriptExecutor jse = (JavascriptExecutor) driver;
+		int y = element.getLocation().getY();
+		jse.executeScript("window.scrollBy(0,"+y+")", element);
+		//jse.executeScript("argument[0].scrollIntoView()", element);
+	}
+}
 
